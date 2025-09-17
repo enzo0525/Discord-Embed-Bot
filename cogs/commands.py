d1f25030a49623ed85e8a817d893d4c25c5fc8f1
enzo0525 <loqqty0525@gmail.com>
2025-09-17 02:22:57 -0400

    new get_links command

---
+++ cogs/commands.py
@@ -0,0 +1,23 @@
+from nextcord.ext import commands
+
+class Commands(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def get_links(self, ctx):
+        await ctx.send('Getting links...')
+
+        links = []
+        channel = ctx.channel
+        async for message in channel.history(limit=100):
+            if message.author == self.bot.user:
+                continue
+            elif message.content.startswith('https://'):
+                links.append(message.content)
+
+        links_str = '\n'.join(links)
+        await ctx.send(f'```{links_str}```')
+
+def setup(bot):
+    bot.add_cog(Commands(bot))
